ad842b5a04b2b6c15e8771d999c73db5d4633f56
Hrishikesh <hrishikeshshendage313@gmail.com>
2026-09-05 14:21:19 +0530

    setting up the models

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from models import InvestigationCase
 
 app = FastAPI()
 
 @app.get("/")
 async def health_check():
     return {"status":"ok"}
+
+@app.post("/investigate")
+async def investigate(case: InvestigationCase):
+    return {
+        "message": "Investigation Case recieved",
+        "case": case
+    }
---
+++ app/models.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+from typing import List, Optional
+
+class Payment(BaseModel):
+    payment_id: str
+    amount: float
+    currency: str
+    status: str
+    method: str
+
+class Dispute(BaseModel):
+    dispute_id: str
+    reason: str
+    customer_claim: str
+
+class MerchantEvidence(BaseModel):
+    order_id: Optional[str] = None
+    product_description: Optional[str] = None
+    delivery_status: Optional[str] = None
+    delivery_date: Optional[str] = None
+    tracking_number: Optional[str] = None
+
+class InvestigationCase(BaseModel):
+    dispute: Dispute
+    payment: Payment
+    merchant_evidence: MerchantEvidence
+    
